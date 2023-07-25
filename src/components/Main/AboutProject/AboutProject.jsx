295d57e37870e1617f3385f21094b247fd34f153
Maiia <maiiapo.com@gmail.com>
2023-07-25 19:38:42 +0500

    Верстка блока about project

--- src/components/Main/AboutProject/AboutProject.jsx
+++ src/components/Main/AboutProject/AboutProject.jsx
@@ -1,8 +1,36 @@
 import './AboutProject.css';
-import React from "react";
+import SectionTitle from "../../сommon/SectionTitle/SectionTitle";
+
 function AboutProject() {
   return (
     <section className="about-project">
+      <SectionTitle title="О проекте"/>
+      <ul className="about-project__description">
+        <li className="about-project__description-item">
+          <h3 className="about-project__description-title">Дипломный проект включал 5 этапов</h3>
+          <p className="about-project__description-text">
+            Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.
+          </p>
+        </li>
+        <li className="about-project__description-item">
+          <h3 className="about-project__description-title">На выполнение диплома ушло 5 недель</h3>
+          <p  className="about-project__description-text">
+            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.
+          </p>
+        </li>
+      </ul>
+      <div className="about-project__duration">
+        <div className="about-project__duration-weeks about-project__duration-weeks_one">
+          1 неделя
+        </div>
+        <div className="about-project__duration-weeks about-project__duration-weeks_four">
+          4 недели
+        </div>
+      </div>
+      <div className="about-project__type">
+        <div className="about-project__type-item">Back-end</div>
+        <div className="about-project__type-item">Front-end</div>
+      </div>
 
     </section>
   )
---
+++ src/components/сommon/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,12 @@
+import './SectionTitle.css';
+
+function SectionTitle({ title }) {
+  return (
+    <div className="section-title">
+      <h2 className="section-title__title">{ title }</h2>
+      <hr />
+    </div>
+  )
+}
+
+export default SectionTitle;
