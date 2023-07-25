8af592d3f1d4249bd7a3a85d04a1e0a50f4b8cb8
Maiia <maiiapo.com@gmail.com>
2023-07-25 18:17:57 +0500

    Доработка header под все разрешения

---
+++ src/components/Main/AboutProject/AboutProject.jsx
@@ -0,0 +1,11 @@
+import './AboutProject.css';
+import React from "react";
+function AboutProject() {
+  return (
+    <section className="about-project">
+
+    </section>
+  )
+}
+
+export default AboutProject;
--- src/components/сommon/Navigation/Navigation.jsx
+++ src/components/сommon/Navigation/Navigation.jsx
@@ -15,53 +15,59 @@ function Navigation() {
 
   return (
     <nav className="nav">
-      <img className="nav__icon" onClick={handleShowMenu} src={Menu} alt="Навигационное меню" />
-      {showMenu &&
-          <div className="nav__dropdown">
-            <img className="nav__close" src={Close} alt="Закрыть меню" onClick={handleShowMenu} />
-            <div className="nav__menu">
-              <Link
-                className={`nav__menu-item ${location.pathname === '/' ? 'nav__menu-item_active' : ''}`}
-                to="/"
-                onClick={handleShowMenu}
-              >
-                Главная
-              </Link>
-              <Link
-                className={`nav__menu-item ${location.pathname === '/movies' ? 'nav__menu-item_active' : ''}`}
-                to="/movies"
-                onClick={handleShowMenu}
-              >
-                Фильмы
-              </Link>
-              <Link
-                className={`nav__menu-item ${location.pathname === '/saved-movies' ? 'nav__menu-item_active' : ''}`}
-                to="/saved-movies"
-                onClick={handleShowMenu}
-              >
-                Сохранённые фильмы
-              </Link>
-            </div>
-            <div className="nav__profile">
-              <Link
-                className="nav__profile-button"
-                to="/profile"
-                onClick={handleShowMenu}
-              >
-                Аккаунт <img className="nav__profile-icon" src={Profile} alt="Аккаунт" />
-              </Link>
-            </div>
-          </div>
-      }
-      {/*<div className="nav__auth">*/}
-      {/*  <Link className="nav__auth-item" to="/signup">*/}
-      {/*    Регистрация*/}
-      {/*  </Link>*/}
+      {location.pathname === '/' ? (
+        <div className="nav__auth">
+          <Link className="nav__auth-item" to="/signup">
+            Регистрация
+          </Link>
 
-      {/*  <Link className="nav__auth-item nav__auth-item_button" to="/signin">*/}
-      {/*    Войти*/}
-      {/*  </Link>*/}
-      {/*</div>*/}
+          <Link className="nav__auth-item nav__auth-item_button" to="/signin">
+            Войти
+          </Link>
+        </div>
+      ) : (
+        <div className="nav__default">
+          <img className="nav__icon" onClick={handleShowMenu} src={Menu} alt="Навигационное меню" />
+          {showMenu &&
+            <div className="nav__dropdown">
+              <img className="nav__close" src={Close} alt="Закрыть меню" onClick={handleShowMenu} />
+              <div className="nav__menu">
+                <Link
+                  className={`nav__menu-item ${location.pathname === '/' ? 'nav__menu-item_active' : ''}`}
+                  to="/"
+                  onClick={handleShowMenu}
+                >
+                  Главная
+                </Link>
+                <Link
+                  className={`nav__menu-item ${location.pathname === '/movies' ? 'nav__menu-item_active' : ''}`}
+                  to="/movies"
+                  onClick={handleShowMenu}
+                >
+                  Фильмы
+                </Link>
+                <Link
+                  className={`nav__menu-item ${location.pathname === '/saved-movies' ? 'nav__menu-item_active' : ''}`}
+                  to="/saved-movies"
+                  onClick={handleShowMenu}
+                >
+                  Сохранённые фильмы
+                </Link>
+              </div>
+              <div className="nav__profile">
+                <Link
+                  className="nav__profile-button"
+                  to="/profile"
+                  onClick={handleShowMenu}
+                >
+                  Аккаунт <img className="nav__profile-icon" src={Profile} alt="Аккаунт" />
+                </Link>
+              </div>
+            </div>
+          }
+        </div>
+      )
+    }
     </nav>
   )
 }
