60eee7b0d6fa793d7ea4bb3cc89221c0bec147e4
Maiia <maiiapo.com@gmail.com>
2023-08-09 21:37:12 +0500

    Поправлена верстка блока main. А также футер.

--- src/components/Main/AboutMe/AboutMe.jsx
+++ src/components/Main/AboutMe/AboutMe.jsx
@@ -12,11 +12,11 @@ function AboutMe() {
           <div className="about-me__description-student">
             <h2 className="about-me__name">Майя</h2>
             <p className="about-me__profession">Фронтенд-разработчик, 35 лет</p>
-            <p className="about-me__info">Я из Перми. В настоящий момент работаю фронтент-разработчиком на Vue.js.
+            <p className="about-me__info">Я из Перми. Работаю фронтент-разработчиком на Vue.js.
               До этого был опыт в бэкенде и фуллстеком. При переходе между стеками была
               необходимость в структурировании знаний, и Яндекс.Практикум помог в этом.
               В свободное время изучаю новые технологии, повышаю уровень английского,
-              хожу в зал и бассейн. Люблю рисовать маслом. Программирование вдохновляет =)
+              хожу в зал и бассейн.
             </p>
           </div>
           <a className="about-me__link" href="https://github.com/MaiiaPo" target="_blank" rel="noreferrer" >
