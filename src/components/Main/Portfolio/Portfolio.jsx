aade587413f63793d36a0675c096469e2322836a
Maiia <maiiapo.com@gmail.com>
2023-08-12 09:24:02 +0500

    Ссылки на лендинге поправлены

--- src/components/Main/Portfolio/Portfolio.jsx
+++ src/components/Main/Portfolio/Portfolio.jsx
@@ -8,21 +8,21 @@ function Portfolio() {
       <ul className="portfolio__links">
         <li className="portfolio__link-item">
           <a className="portfolio__link" href="https://github.com/MaiiaPo/how-to-learn" target="_blank" rel="noreferrer">
-            Статичный сайт
+            <span className="portfolio__link-name">Статичный сайт</span>
+            <img className="portfolio__arrow" src={Arrow} alt="Ссылка на сайт" />
           </a>
-          <img className="portfolio__arrow" src={Arrow} alt="Ссылка на сайт" />
         </li>
         <li className="portfolio__link-item">
           <a className="portfolio__link" href="https://maiiapo.github.io/russian-travel/" target="_blank" rel="noreferrer">
-            Адаптивный сайт
+            <span className="portfolio__link-name">Адаптивный сайт</span>
+            <img className="portfolio__arrow" src={Arrow} alt="Ссылка на сайт" />
           </a>
-          <img className="portfolio__arrow" src={Arrow} alt="Ссылка на сайт" />
         </li>
         <li className="portfolio__link-item">
           <a className="portfolio__link" href="https://maiiapo.github.io/mesto/" target="_blank" rel="noreferrer">
-            Одностраничное приложение
+            <span className="portfolio__link-name">Одностраничное приложение</span>
+            <img className="portfolio__arrow" src={Arrow} alt="Ссылка на сайт" />
           </a>
-          <img className="portfolio__arrow" src={Arrow} alt="Ссылка на сайт" />
         </li>
       </ul>
     </section>
